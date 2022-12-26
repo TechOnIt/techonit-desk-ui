03caa04cc8a868e52565c6c1cd3e93e9e8b1a117
mohsen.mahv <mohsen.mahv@gmail.com>
2022-12-26 17:36:46 +0330

    new commit

--- techonit-desk/pages/_app.js
+++ techonit-desk/pages/_app.js
@@ -2,18 +2,22 @@ import Head from 'next/head'
 import AuthLayout from '../components/auth/layouts/layout'
 import MainLayout from '../components/main/layouts/layout'
 import ManageLayout from '../components/manage/layouts/layout'
+import combineReducers from '../reducers'
+import Provider from "react-redux"
+import createStore from "redux"
 
 import '../styles/globals.css'
 import '../components/manage/layouts/vertical-layout.css'
 import "../styles/libs/font-awesome/css/fontawesome.min.css"
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   // set layout
   const Layout = Component.layout === 'auth' ? AuthLayout :
     Component.layout === 'manage' ? ManageLayout : MainLayout;
-
+console.log("red",combineReducers)
   return (
     <>
+    <Provider store={createStore(combineReducers)=>{}}>
       <Head>
         <title>{Component.title ? Component.title + ' - ' : ''}Technology On It</title>
         if(Component.description){
@@ -22,13 +26,14 @@ function MyApp({ Component, pageProps }) {
         <meta
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
-        />
+          />
       </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+          </Provider>
     </>
   )
 }
 
-export default MyApp
+export default App
